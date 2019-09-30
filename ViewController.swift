1a9ff38ee9a8da1b5c6f6c5d81630273b9d90585
Eugene <kapust@ukr.net>
2019-09-30 15:53:05 +0300

    adding blanc proj

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostFetchingApp
+//
+//  Created by SKC on 9/30/19.
+//  Copyright © 2019 SKC-PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
